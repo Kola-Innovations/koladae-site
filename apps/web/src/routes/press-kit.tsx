5358be0f9223a0de35286f5b548749bb654de9e3
Tobi <37726406+akinwol@users.noreply.github.com>
2025-12-10 22:50:11 -0500

    update press kit hero

--- apps/web/src/routes/press-kit.tsx
+++ apps/web/src/routes/press-kit.tsx
@@ -1,3 +1,4 @@
+import PressKitHero from "@/components/press-kit-hero";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/press-kit")({
@@ -5,5 +6,12 @@ export const Route = createFileRoute("/press-kit")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/press-kit"!</div>;
+  return (
+    <div>
+      <PressKitHero
+        artistName="KOLADAE"
+        imageUrl="https://plus.unsplash.com/premium_photo-1683299265520-928021064f4c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fG11c2ljJTIwYXJ0aXN0fGVufDB8fDB8fHww"
+      />
+    </div>
+  );
 }
