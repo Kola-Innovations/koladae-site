4c2ebf62481668e42262fb87383c522ebd97e7aa
Tobi <37726406+akinwol@users.noreply.github.com>
2025-12-12 15:39:43 -0500

    updated about section

--- apps/web/src/routes/press-kit.tsx
+++ apps/web/src/routes/press-kit.tsx
@@ -18,7 +18,7 @@ function RouteComponent() {
         artistName="KOLADAE"
         imageUrl="https://plus.unsplash.com/premium_photo-1683299265520-928021064f4c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fG11c2ljJTIwYXJ0aXN0fGVufDB8fDB8fHww"
       />
-      <AboutSection />
+      <AboutSection bgColor="cream" />
       <SongSection videoUrl={video3} reversed />
       <SongSectionStacked />
       <SongSectionHero />
