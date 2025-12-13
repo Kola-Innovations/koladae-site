bcba7220eb5f6a9ee4dc3f5e979766834a8ba4d4
Tobi <37726406+akinwol@users.noreply.github.com>
2025-12-13 15:57:00 -0500

    updateD

--- apps/web/src/components/nav.tsx
+++ apps/web/src/components/nav.tsx
@@ -39,7 +39,7 @@ function MainNav() {
         }
       `}</style>
       <nav
-        className="absolute top-12 flex w-full justify-center gap-12"
+        className="absolute left-0 right-0 top-12 flex justify-center gap-12"
         style={{
           animation: "fadeIn 1s ease forwards",
           animationDelay: "0.5s",
